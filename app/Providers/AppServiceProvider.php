10f0a858cb2b7222ebf9f8987c726558fd55abec
Yota Yamamoto <yota4807@gmail.com>
2024-07-01 11:18:58 +0900

    ログイン機能とブログの作成

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\URL;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -20,5 +22,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        URL::forceScheme('https');
     }
 }
